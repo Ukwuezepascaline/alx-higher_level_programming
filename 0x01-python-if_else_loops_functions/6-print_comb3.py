0f64c8c2d29bf522d9c8f1c9c2ea2f91cd7b2913
Ukwuezepascaline <ezikepascaline@gmail.com>
2022-11-30 22:36:54 +0100

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-    for i in range(10):
-         for j in range(10):
-             if(i == 8 and j == 9):
-                  print("{}".format(str(i) + str(j)))
-              elif(j > i):
-                  print("{}".format(str(i) + str(j)) + ", ", end="")
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i >= j:
+            continue
+        elif i == 8 and j == 9:
+            print('{}{}'.format(i, j))
+        else:
+            print('{}{}, '.format(i, j), end="")
