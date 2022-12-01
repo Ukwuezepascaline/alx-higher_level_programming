24d0a278b3e134f20f3ec0039fa2e73da577d612
Ukwuezepascaline <ezikepascaline@gmail.com>
2022-12-01 06:00:51 +0100

    Update 11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    print("{:c}".format(i - 32) if i % 2 != 0 else "{:c}".format(i), end="")
+import sys
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)
